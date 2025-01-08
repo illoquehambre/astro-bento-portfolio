a9bca292a8d01db84b75fbe3e271d054e1cd9a15
Ignacio Moreno <illoquehambre@gmail.com>
2025-01-08 19:00:37 +0100

    update to astroV5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,19 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel/serverless";
 import robotsTxt from "astro-robots-txt";
 import UnoCSS from "@unocss/astro";
 import icon from "astro-icon";
 import solidJs from "@astrojs/solid-js";
-import svelte from "@astrojs/svelte";
 import events from 'events';
 
+import vercel from "@astrojs/vercel";
+
 // Incrementa el límite global de oyentes
-events.setMaxListeners(50); // Cambia el número según sea necesario
+//events.setMaxListeners(50); // Cambia el número según sea necesario
 
 export default defineConfig({
   site: "https://ignaciomg.xyz/",
+
   integrations: [
     sitemap(),
     robotsTxt({
@@ -31,7 +32,6 @@ export default defineConfig({
             '**/*.astro',
             '**/*.tsx',
             '**/*.jsx',
-            '**/*.svelte',
           ],
           exclude: [
             'node_modules/**',
@@ -42,17 +42,18 @@ export default defineConfig({
       },
     }),
     icon(),
-    svelte(),
   ],
-  output: "server",
-  adapter: vercel(),
+
+  // Asegúrate de cambiar esta línea
+  output: "static",
+  adapter: vercel(),  // Aunque el adaptador siga aquí, cambiar a static elimina las funciones serverless
   vite: {
     server: {
       watch: {
-        usePolling: true, // Cambia a vigilancia basada en sondeo.
-        interval: 300,    // Aumenta el intervalo entre verificaciones para evitar una carga alta.
+        usePolling: true,
+        interval: 300,
       },
     },
-   
   },
+
 });
